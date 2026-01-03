6926134c30b116a6a755a16aeb813179b848616b
Nick Sweet <nickwsweet@gmail.com>
2026-01-03 00:57:56 -0800

    Fix photo details styling

--- src/overlays/Photos/PhotoDetail.jsx
+++ src/overlays/Photos/PhotoDetail.jsx
@@ -6,7 +6,19 @@ export default function PhotoDetail({ photo, isOpen, onExpand, onClose }) {
   return(
     <div className="photo-detail">
       <img src={photo}/>
-      <motion.h4 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={onClose}>back to grid</motion.h4>
+      <div className='photo-caption'>
+        <h4>Title</h4>
+        <p>Location: Somewhere</p>
+        <p>Camera: Probably an iPhone</p>
+      </div>
+      <motion.h4 
+        whileHover={{ scale: 1.1 }} 
+        whileTap={{ scale: 0.9 }} 
+        style={{ transformOrigin: 'right' }}
+        onClick={onClose}
+      >
+        back to grid
+      </motion.h4>
     </div>
   );
 }
